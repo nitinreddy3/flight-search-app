79acc0ede4be5c649f05ea6b206b1a16c9f04d22
nitin reddy <reddynitin3@gmail.com>
2018-09-29 20:59:15 +0530

    Created responsive component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import styled from 'styled-components';
 
 
+function getWidthString(span) {
+  if(!span) return;
+
+  let width = span / 12 * 100;
+  return `width: ${width}%`;
+}
+
 const Row = styled.div`
   &::after {
     content: "";
@@ -14,27 +21,29 @@ const Row = styled.div`
 
 const Col = styled.div`
   float: left;
-  width: 100;
+  ${({xs}) => (xs ? getWidthString(xs) : "width: 100%")};
+  
 
   @media only screen and (min-width: 768px) {
-    width: ${props => (props.span ? props.span / 12 * 100 : "8.33")}%;
+    ${({sm}) => sm && getWidthString(sm)};
+  }
+
+  @media only screen and (min-width: 992px) {
+    ${({md}) => md && getWidthString(md)};
+  }
+
+  @media only screen and (min-width: 1200px) {
+    ${({lg}) => lg && getWidthString(lg)};
   }
 `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
+      <div>
         <Row>
-          <Col span="6">Hello1</Col>
-          <Col span="6">Hello2</Col>
+          <Col xs="12" sm="6" md="8">md: 8 - sm: 6 - xs: 12</Col>
+          <Col xs="6" md="4">md: 4 - xs: 6</Col>
         </Row>
       </div>
     );
